33113bbb3846a942cad2baac1f565f8441276ac7
Laksumi Soma <laksumi2202@gmail.com>
2023-03-12 16:44:09 -0400

    conditions

--- NextTest/src/main/java/com/ontariotechu/sofe3980U/NextDate.java
+++ NextTest/src/main/java/com/ontariotechu/sofe3980U/NextDate.java
@@ -7,6 +7,16 @@ public class NextDate {
     private int month;
     private int year;
     public static String Nextdate(int day, int month, int year){
+        String returnDate;
+
+        if(year < 1812 || year > 2212){
+            System.out.println("This is not a valid year");
+            return "This is not a valid year";
+        }
+        if (month < 1 || month > 12){
+            System.out.println("This is not a valid month");
+            return "This is not a valid month";
+        }
 
     }
 
