c66522578c65c5d99400449f06092b84e6e95889
Laksumi Soma <laksumi2202@gmail.com>
2023-03-12 16:38:48 -0400

    next date

--- NextTest/src/main/java/com/ontariotechu/sofe3980U/NextDate.java
+++ NextTest/src/main/java/com/ontariotechu/sofe3980U/NextDate.java
@@ -3,5 +3,11 @@ package com.ontariotechu.sofe3980U;
 
 public class NextDate {
 
-    
+    private int day;
+    private int month;
+    private int year;
+    public static String Nextdate(int day, int month, int year){
+
+    }
+
 }
