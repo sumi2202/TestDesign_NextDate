cc681d8aae446a67d36c63e13d64e031fd853d37
Laksumi Soma <laksumi2202@gmail.com>
2023-03-12 17:10:55 -0400

    .

--- NextTest/src/main/java/com/ontariotechu/sofe3980U/NextDate.java
+++ NextTest/src/main/java/com/ontariotechu/sofe3980U/NextDate.java
@@ -7,15 +7,15 @@ public class NextDate {
 
         if(year < 1812 || year > 2212){
             System.out.println("This is not a valid year");
-            return "This is not a valid year";
+            return "This is not a valid date";
         }
         if (month < 1 || month > 12){
             System.out.println("This is not a valid month");
-            return "This is not a valid month";
+            return "This is not a valid date";
         }
         if (day < 1 || day > 31){
             System.out.println("This is not a valid day");
-            return "This is not a valid day";
+            return "This is not a valid date";
         }
 
 
