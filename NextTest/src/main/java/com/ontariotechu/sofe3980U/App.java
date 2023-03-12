a1e3d41b01449a42b992f525728753cae8acd980
Laksumi Soma <laksumi2202@gmail.com>
2023-03-12 17:08:50 -0400

    .

--- NextTest/src/main/java/com/ontariotechu/sofe3980U/App.java
+++ NextTest/src/main/java/com/ontariotechu/sofe3980U/App.java
@@ -9,11 +9,14 @@ public class App
 {
     public static void main( String[] args )
     {
+        NextDate inputDate = new NextDate();
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the date in numerical form in the following order:\nyear (yyyy)\nmonth (mm)\nday (dd)\nEnter after every input.");
 
         int year = input.nextInt();
         int month = input.nextInt();
         int day = input.nextInt();
+
+        System.out.println("The next date is: " + NextDate.Date(year, month, day));
     }
 }
