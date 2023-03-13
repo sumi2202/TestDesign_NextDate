1547a03fe82d285165327bbe7f433c233ab5623f
Laksumi Soma <laksumi2202@gmail.com>
2023-03-12 21:05:32 -0400

    .

--- NextTest/src/main/java/com/ontariotechu/sofe3980U/NextDate.java
+++ NextTest/src/main/java/com/ontariotechu/sofe3980U/NextDate.java
@@ -25,7 +25,14 @@ public class NextDate {
             yearLeap = true;
         }
         //ensuring that the months that have 30 days and the months that have 31 days are accounted for
-        if()
+        if((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10) && day == 31){
+            month = month + 1;
+            day = 1;
+        }
+        if((month == 4 || month == 6 || month == 9 || month == 11 ) && day == 31){
+            month = month + 1;
+            day = 1;
+        }
 
 
 
